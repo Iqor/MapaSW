9f7293d23e0687569e7fd7a38de6c9f27376ad7f
Igor <iqorrodrigues@gmail.com>
2017-11-16 18:47:11 -0300

    Teste com a requisição no servidor do TCM.

--- main/js/code.js
+++ main/js/code.js
@@ -4,6 +4,96 @@
 //
 //////////////////////////////
 
+
+$( document ).ready(function() {
+var myData;
+
+  $.ajax({
+      url: "http://api.tcm.ce.gov.br/sim/1_0/municipios.json",
+      type: "GET",
+      data:  ,
+      dataType: "json",
+      crossDomain: true,
+
+      headers: {
+               "Accept" : "application/json; charset=utf-8",
+               "Content-Type": "application/javascript; charset=utf-8",
+               "Access-Control-Allow-Origin" : "*"
+    },
+
+  }).done(function(resposta) {
+      console.log(resposta);
+
+  }).fail(function(jqXHR, textStatus ) {
+      console.log("Request failed: " + textStatus);
+
+  }).always(function() {
+      console.log("completou");
+  });
+
+
+
+
+
+
+
+
+/*
+  $.get("http://api.tcm.ce.gov.br/sim/1_0/municipios.xml", function(resultado){
+    //$("#mensagem").html(resultado);
+      console.log(resultado);
+
+   })
+*/
+
+/*
+// Using the core $.ajax() method
+$.ajax({
+
+    // The URL for the request
+    url: "http://api.tcm.ce.gov.br/sim/1_0/municipios.json",
+
+    // The data to send (will be converted to a query string)
+    data: {
+        id: 123
+    },
+
+    // Whether this is a POST or GET request
+    type: "GET",
+
+    // The type of data we expect back
+    dataType : "json",
+})
+  // Code to run if the request succeeds (is done);
+  // The response is passed to the function
+  .done(function( json ) {
+     $( "<h1>" ).text( json.title ).appendTo( "body" );
+     $( "<div class=\"content\">").html( json.html ).appendTo( "body" );
+  })
+  // Code to run if the request fails; the raw request and
+  // status codes are passed to the function
+  .fail(function( xhr, status, errorThrown ) {
+    alert( "Sorry, there was a problem!" );
+    console.log( "Error: " + errorThrown );
+    console.log( "Status: " + status );
+    console.dir( xhr );
+  })
+  // Code to run regardless of success or failure;
+  .always(function( xhr, status ) {
+    alert( "The request is complete!" );
+  });
+
+
+*/
+});
+
+
+
+
+
+
+
+
 for (var i = 0; i < cidades.length; i++) {
 
 	cidades[i].mouseover(function(e){
