18e74887e3ed9cf2e4de629f82cc680e418399c1
Davi Tabosa <davitabosa12@gmail.com>
2017-12-01 17:51:16 -0300

    funcoes de busca

--- main/js/davi.js
+++ main/js/davi.js
@@ -1,11 +1,9 @@
 function getMunicipios(atributos, callback){
-    var url = "/server/padder.php?metodo=municipios&nome_municipio=" + nome_municipio;
-    var resto;
+    var url = "/server/padder.php?metodo=municipios&nome_municipio=";
     for(var key in atributos){
 
-        resto += "&" + key + "=" + atributos[key];
+        url += "&" + key + "=" + atributos[key];
     }
-    url += resto;
     $.getJSON(url,callback);
 }
 
@@ -16,33 +14,60 @@ function getMunicipios(atributos, callback){
  */
 function getFuncao(atributos, callback){
     var url = "/server/padder.php?metodo=funcoes";
-    var resto;
     for(var key in atributos){
 
-        resto += "&" + key + "=" + atributos[key];
+        url += "&" + key + "=" + atributos[key];
     }
-    url += resto;
     $.getJSON(url,callback);
 }
-function getDespesaProjetoAtividade(atributos,callback){
-    var url = "/server/padder.php?metodo=despesa_projeto_atividade";
-    var resto;
+function getDespesaProjetoAtividade(atributos,ano,callback){
+    var url = "/server/padder.php?metodo=despesa_projeto_atividade&exercicio_orcamento=" + ano + "00";
     for(var key in atributos){
 
-        resto += "&" + key + "=" + atributos[key];
+        url += "&" + key + "=" + atributos[key];
     }
-    url += resto;
     $.getJSON(url,callback);
 
 }
+
+/**
+ * 
+ * @param {string} searchKey O valor a ser procurado
+ * @param {string} collumn A coluna onde o valor está
+ * @param {JSON} dataset A coleção de dados
+ */
+function searchInArray(searchKey,collumn, dataset){
+    var result = [];
+    for(var value in dataset){
+        if(dataset[value][collumn] === searchKey){
+            result.push(dataset[value]);
+        }
+    }
+    return result;
+}
+
+function teste2(){
+    getFuncao({},dados=>{
+        var lol;
+        lol = searchInArray(" Saúde","nome_funcao",dados.rsp._content);
+        console.log(lol);
+    })
+}
+
 function teste(){
     getFuncao({nome_funcao: "edu"},dados =>{
+        console.log(dados);
         var cod_edu  = dados.rsp._content[0].codigo_funcao;
         getMunicipios({nome_municipio:"fortaleza"},dados=>{
+            console.log(dados);
             var cod_fortaleza = dados.rsp._content[0].codigo_municipio;
-            getDespesaProjetoAtividade({codigo_funcao: cod_edu, codigo_municipio: cod_fortaleza},
-            dados=>{
-                console.log(dados);
+            console.log(cod_edu +  " " + cod_fortaleza);
+            getDespesaProjetoAtividade({codigo_funcao: cod_edu,codigo_municipio:cod_fortaleza},2017,dados=>{
+                var total = 0;
+                for(var i = 0; i < dados.rsp._content.length; i++){
+                    total += Number(dados.rsp._content[i].valor_total_fixado_projeto_atividade);
+                }
+                console.log(total + " gastos em Educacao");
             });
         });
     });
